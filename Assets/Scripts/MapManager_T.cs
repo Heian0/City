59abab852649b50ac48f704f9e5d59ea25098be4
Heian0 <77514857+Heian0@users.noreply.github.com>
2023-05-31 19:12:07 -0700

    can expand paths now

--- Assets/Scripts/MapManager_T.cs
+++ Assets/Scripts/MapManager_T.cs
@@ -9,6 +9,9 @@ public class MapManager_T : MonoBehaviour
     private Tilemap map;
 
     [SerializeField]
+    private Tilemap buildingMap;
+
+    [SerializeField]
     private List<TileData> tileDatas;
 
     public Dictionary<TileBase, TileData> dataFromBase = new Dictionary<TileBase, TileData>();
@@ -34,31 +37,107 @@ public class MapManager_T : MonoBehaviour
 
     private void Start()
     {
+        //Gotta fix this dogshit system eventually
         tileDict_tl.Add((200, 100), 201);
+        tileDict_tl.Add((200, 202), 202);
+        tileDict_tl.Add((200, 200), 200);
+        tileDict_tl.Add((200, 210), 210);
         tileDict_tl.Add((200, 203), 202);
+        tileDict_tl.Add((200, 208), 208);
+        tileDict_tl.Add((200, 204), 210);
+        tileDict_tl.Add((200, 207), 208);
+        tileDict_tl.Add((200, 212), 212);
+        tileDict_tl.Add((200, 211), 200);
+        tileDict_tl.Add((200, 209), 209);
+        tileDict_tl.Add((200, 206), 212);
         tileDictList.Add(tileDict_tl);
 
         tileDict_t.Add((200, 100), 202);
+        tileDict_t.Add((200, 203), 202);
+        tileDict_t.Add((200, 201), 202);
+        tileDict_t.Add((200, 204), 210);
+        tileDict_t.Add((200, 208), 209);
+        tileDict_t.Add((200, 200), 200);
+        tileDict_t.Add((200, 212), 200);
+        tileDict_t.Add((200, 211), 200);
+        tileDict_t.Add((200, 206), 200);
         tileDictList.Add(tileDict_t);
 
         tileDict_tr.Add((200, 100), 203);
+        tileDict_tr.Add((200, 202), 202);
+        tileDict_tr.Add((200, 209), 209);
+        tileDict_tr.Add((200, 200), 200);
+        tileDict_tr.Add((200, 203), 202);
+        tileDict_tr.Add((200, 204), 204);
+        tileDict_tr.Add((200, 201), 202);
+        tileDict_tr.Add((200, 210), 210);
+        tileDict_tr.Add((200, 211), 211);
+        tileDict_tr.Add((200, 205), 204);
+        tileDict_tr.Add((200, 206), 211);
+        tileDict_tr.Add((200, 208), 209);
+        tileDict_tr.Add((200, 212), 200);
         tileDictList.Add(tileDict_tr);
 
         tileDict_r.Add((200, 100), 204);
+        tileDict_r.Add((200, 200), 200);
+        tileDict_r.Add((200, 203), 204);
+        tileDict_r.Add((200, 202), 210);
+        tileDict_r.Add((200, 205), 204);
+        tileDict_r.Add((200, 212), 200);
+        tileDict_r.Add((200, 206), 211);
+        tileDict_r.Add((200, 209), 200);
+        tileDict_r.Add((200, 211), 200);
         tileDictList.Add(tileDict_r);
 
         tileDict_br.Add((200, 100), 205);
+        tileDict_br.Add((200, 206), 206);
+        tileDict_br.Add((200, 204), 204);
+        tileDict_br.Add((200, 210), 210);
+        tileDict_br.Add((200, 200), 200);
+        tileDict_br.Add((200, 203), 204);
+        tileDict_br.Add((200, 207), 206);
+        tileDict_br.Add((200, 212), 212);
+        tileDict_br.Add((200, 211), 211);
+        tileDict_br.Add((200, 209), 200);
+        tileDict_br.Add((200, 202), 210);
         tileDictList.Add(tileDict_br);
 
         tileDict_b.Add((200, 100), 206);
+        tileDict_b.Add((200, 205), 206);
+        tileDict_b.Add((200, 207), 206);
+        tileDict_b.Add((200, 200), 200);
+        tileDict_b.Add((200, 210), 200);
+        tileDict_b.Add((200, 204), 211);
+        tileDict_b.Add((200, 208), 212);
+        tileDict_b.Add((200, 203), 211);
+        tileDict_b.Add((200, 209), 200);
+        tileDict_b.Add((200, 202), 200);
         tileDictList.Add(tileDict_b);
 
         tileDict_bl.Add((200, 100), 207);
+        tileDict_bl.Add((200, 206), 206);
+        tileDict_bl.Add((200, 208), 208);
+        tileDict_bl.Add((200, 200), 200);
+        tileDict_bl.Add((200, 209), 209);
+        tileDict_bl.Add((200, 201), 208);
+        tileDict_bl.Add((200, 212), 212);
+        tileDict_bl.Add((200, 210), 200);
+        tileDict_bl.Add((200, 211), 211);
         tileDict_bl.Add((200, 205), 206);
+        tileDict_bl.Add((200, 204), 211);
+        tileDict_bl.Add((200, 202), 209);
         tileDictList.Add(tileDict_bl);
 
         tileDict_l.Add((200, 100), 208);
         tileDict_l.Add((200, 200), 200);
+        tileDict_l.Add((200, 201), 208);
+        tileDict_l.Add((200, 202), 209);
+        tileDict_l.Add((200, 210), 200);
+        tileDict_l.Add((200, 207), 208);
+        tileDict_l.Add((200, 206), 212);
+        tileDict_l.Add((200, 204), 200);
+        tileDict_l.Add((200, 211), 200);
+        tileDict_l.Add((200, 205), 212);
         tileDictList.Add(tileDict_l);
     }
 
@@ -104,6 +183,8 @@ public class MapManager_T : MonoBehaviour
         map.SetTile(bl_pos, getTileWithID(bl_id));
         map.SetTile(l_pos, getTileWithID(l_id));
     }
+
+    public void placeBuilding
 
     public TileBase getTileWithID(int id)
     {
