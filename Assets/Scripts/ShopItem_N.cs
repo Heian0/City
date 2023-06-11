632ae6cd8e7d14527d40222c2f3d82702d6932e3
Nicholas Liang <gamesfoundationfun@gmail.com>
2023-06-10 23:27:41 -0700

    made the shop items prefabs, added new shop items

--- Assets/Scripts/ShopItem_N.cs
+++ Assets/Scripts/ShopItem_N.cs
@@ -10,20 +10,27 @@ public class ShopItem_N : MonoBehaviour
     [SerializeField]
     private int itemPrice;
     [SerializeField]
-    private string itemName;
+    private TMP_Text itemName;
     [SerializeField]
     private Image itemArt;
     [Header("References")]
     public TMP_Text priceTxt;
     public TMP_Text nameTxt;
     public Image itemImage;
+    public GameObject buyPanel;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        priceTxt = GameObject.Find("Price").GetComponent<TMP_Text>();
+        nameTxt = GameObject.Find("Item Name").GetComponent<TMP_Text>();
+        itemImage = GameObject.Find("Image").GetComponent<Image>();
     }
 
+    void Awake()
+    {
+        //buyPanel = GameObject.Find("Item Stats");
+    }
     // Update is called once per frame
     void Update()
     {
@@ -32,8 +39,9 @@ public class ShopItem_N : MonoBehaviour
 
     public void DisplayItem()
     {
+        //buyPanel.SetActive(true);
         priceTxt.text = itemPrice.ToString() + " Gold";
-        nameTxt.text = itemName;
+        nameTxt.text = itemName.text;
         itemImage.sprite = itemArt.sprite;
     }
 }
