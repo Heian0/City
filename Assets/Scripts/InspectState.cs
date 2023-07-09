340b0fc0cb59ab3c11556776e419c8762747fe1f
Heian0 <77514857+Heian0@users.noreply.github.com>
2023-07-08 19:39:06 -0700

    stuff

--- Assets/Scripts/InspectState.cs
+++ Assets/Scripts/InspectState.cs
@@ -18,11 +18,10 @@ public class InspectState : GameBaseState
         if (Input.GetMouseButtonDown(0))
         {
             var rayHit = Physics2D.GetRayIntersection(metaData.cam.ScreenPointToRay(Input.mousePosition));
-            if (!rayHit.collider) return;
 
-            Debug.Log(rayHit.collider.gameObject);
-            if(rayHit.collider.gameObject.GetComponent<InteractableBuilding_N>() != null)
+            if (rayHit.collider && rayHit.collider.gameObject.GetComponent<InteractableBuilding_N>() != null)
             {
+                Debug.Log(rayHit.collider.gameObject);
                 if (metaData.selectedGO != null)
                 {
                     metaData.selectedGO.GetComponent<InteractableBuilding_N>().isSelected = false;
@@ -33,7 +32,26 @@ public class InspectState : GameBaseState
                 metaData.selectedGO.GetComponent<InteractableBuilding_N>().isSelected = true;
                 metaData.selectedBuildingImage.sprite = rayHit.collider.gameObject.GetComponent<SpriteRenderer>().sprite;
             }
+
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            var tpos = metaData.map.WorldToCell(worldPoint);
+
+            // Try to get a tile from cell position
+            var tile = metaData.map.GetTile(tpos);
+
+            Debug.Log(tile);
+
+            if (tile)
+            {
+                metaData.inspectScreen.SetActive(true);
+                metaData.selectedBuildingText.text = metaData.selectedName;
+                metaData.selectedGO = rayHit.collider.gameObject;
+                metaData.selectedGO.GetComponent<InteractableBuilding_N>().isSelected = true;
+                metaData.selectedBuildingImage.sprite = rayHit.collider.gameObject.GetComponent<SpriteRenderer>().sprite;
+            }
         }
+
     }
 
 }
